cb5b58dbfa12d0701e346ce1db50e6ed2af2a414
Esteban Cabal <estcab@gmail.com>
2018-10-27 18:04:12 +0200

    Add Data with EF Core

---
+++ PieShop/Models/PieRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PieRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _context.Pies.ToList();
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _context.Pies.FirstOrDefault(p => p.Id.Equals(pieId));
+        }
+    }
+}
