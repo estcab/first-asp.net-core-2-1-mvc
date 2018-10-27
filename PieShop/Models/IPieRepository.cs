e3be21fc267fa0ff86ae6ae002d2d1517f66c835
Esteban Cabal <estcab@gmail.com>
2018-10-27 10:48:41 +0200

    Create the model and mock repository

---
+++ PieShop/Models/IPieRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
+    }
+}
